fb1c77fc1aa13a2727d76bfa0099696ed6443c72
mattias <mattias.sheehan@outlook.com>
2025-10-18 18:53:18 -0600

    finished timer

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -1,16 +1,25 @@
 
 $(function() {
-    let countMain=270;
-    let countMin=0;
-    let countRing=0;
-    let ring1 = true;
-    let ring2 = true;
-    let countingRing = false;
-    let displayStringMain = " until ring 1 starts"
-    let displayStringRing = " until ring 1 closes"
+
     let timerInt = null;
 
-    timerInt = setInterval(() => {
+    const speed = 1000
+
+    startTimer(265,"Goodluck!",true);
+
+    function startTimer(startTime,endMessage,resetBool) {
+            let countMain=startTime;
+            let countRing=0;
+            let ring1 = true;
+            let ring2 = true;
+            let countingRing = false;
+            let displayStringMain = "Ring 1 Starts In: "
+            let displayStringRing = "Ring 1 Closes in: "
+
+            $("#ringStart").html("")
+            $("#ringClose").html("")
+            $("#timerReset").html("")
+        timerInt = setInterval(() => {
         //display timer
         countMain--;
 
@@ -21,51 +30,63 @@ $(function() {
 
         //if either ring is true, update 
         if (ring1 || ring2) {
-            $("#timerTest").html(String(Math.floor(countMain/60)).padStart(2,"0") + ":" + String(countMain%60).padStart(2,"0")+displayStringMain)
+            $("#ringStart").html(displayStringMain+String(Math.floor(countMain/60)).padStart(2,"0") + ":" + String(countMain%60).padStart(2,"0"))
         }
-
-
-        //fix this somehow
+        
         if(countingRing && countRing !== 0) {
-            $("#ringTimer").html(String(Math.floor(countRing/60)).padStart(2,"0") + ":" + String(countRing%60).padStart(2,"0")+displayStringRing)
+            $("#ringClose").html(displayStringRing+String(Math.floor(countRing/60)).padStart(2,"0") + ":" + String(countRing%60).padStart(2,"0"))
         }
 
         if(countRing === 0) {
-            $("#ringTimer").html("")
+            $("#ringClose").html("")
         }
         
 
         //once ring1 is done, set time to 6:30 and ring counter to 2:00
         if (countMain === 0 && ring1) {
             countMain=390
-            countRing=120;
+            countRing=180;
             ring1=false; //false ring flag
-            displayStringMain = " until ring 2 starts"
+            displayStringMain = "Ring 2 Starts In: "
             countingRing = true;
         }
         //if ring2 is done, set ring timer to 3:00
         if (countMain === 0 && !ring1) {
             ring2=false; //false ring2 flag
             countRing=180; //3:00
-            displayStringRing = " until ring 2 closes"
-            $("#timerTest").html("")
+            displayStringRing = "Ring 2 Closes in: "
+            $("#ringStart").html("")
 
         }
 
         if (countRing === 0 && !ring2) {
             clearInterval(timerInt);
-            $("#ringTimer").html("Good luck!")
+            $("#ringStart").html(endMessage)
+            $("#ringClose").html("")
+            if (resetBool) {
+                $("#timerReset").html("(Reset)")
+            }
+            timerInt = null;
         }
 
         
 
-    },10);
+        },speed);
+    }
+
+    $('#timerReset')
+    .on('mouseup mouseleave touchend touchcancel', function() { $(this).removeClass('press'); })
+    .on('click', function() {
+        startTimer(300,"winner winner chicken dinner?",false); 
+    });
 
 
     $('#button2').on('click', function() {
         clearInterval(timerInt);
         console.log("timer stopped")
     })
+
+    
 
 });
 
